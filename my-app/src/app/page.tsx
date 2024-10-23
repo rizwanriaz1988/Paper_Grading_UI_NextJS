e867848302474b0f3b22506561652b18ecd69a9f
Rizwan Riaz <139876959+rizwanriaz1988@users.noreply.github.com>
2024-10-23 23:41:43 +0500

    wwwq1

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Savssssssssssssssssssssssssssinstantly.</li>
+          <li>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
