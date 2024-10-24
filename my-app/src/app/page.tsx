dcab4ab02009c14341ec0f1b5c522cd6e35552ea
Rizwan Riaz <139876959+rizwanriaz1988@users.noreply.github.com>
2024-10-24 20:42:26 +0500

    Rizwan

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Riz.</li>
+          <li>Rizwan.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
